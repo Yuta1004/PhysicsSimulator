c295ddc9c549c792175d797ae543a979a7a1886a
NakagamiYuta <guguru0014@gmail.com>
2022-04-24 23:59:47 +0900

    [add](core) CountUpSimulator(テスト用)を書いた

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,1 +1,22 @@
 pub mod memory;
+pub mod simulator {
+    pub mod countup;
+}
+
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+use memory::memory::MemManager;
+use simulator::countup::CountUpSimulator;
+
+#[wasm_bindgen]
+struct SimulatorFactory;
+
+#[wasm_bindgen]
+impl SimulatorFactory {
+    pub fn new_countup_simulator(blocks_num: i32, steps_num: i32) -> MemManager {
+        let countup_simulator = Box::new(CountUpSimulator::new());
+        MemManager::new(blocks_num, steps_num, countup_simulator)
+    }
+}
