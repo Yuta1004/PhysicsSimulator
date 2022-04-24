4648e053b3a04319a0b7fefc70af78efd82245e3
NakagamiYuta <guguru0014@gmail.com>
2022-04-25 00:02:01 +0900

    [add](front) CountUpSimulatorをReact側から実行して値を確認できるようにした

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,14 @@
 import init from "@kepler-core/kepler-core";
+import { SimulatorFactory, InitOutput } from "@kepler-core/kepler-core";
 
 const App = () => {
-  init().then(() => {});
+  init().then((instance: InitOutput) => {
+    const countup_simulator = SimulatorFactory.new_countup_simulator(6, 10);
+    const shared_memory = new Float64Array(instance.memory.buffer, countup_simulator.as_mem_ptr(), 60);
+    for(var idx = 0; idx < 60; ++ idx) {
+      console.log(shared_memory[idx]);
+    }
+  });
 
   return (
     <div>
