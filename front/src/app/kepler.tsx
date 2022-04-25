dfb49b9d67efbb4312c4a8e9f0cd343873b0a4f4
NakagamiYuta <guguru0014@gmail.com>
2022-04-26 00:13:09 +0900

    [update](front) CountUpSimulatorを使ってとりあえず新しく書いたコードを試している状態に

--- front/src/app/kepler.tsx
+++ front/src/app/kepler.tsx
@@ -1,13 +1,21 @@
 import init from "@kepler-core/kepler-core";
 import { SimulatorFactory, InitOutput } from "@kepler-core/kepler-core";
 
+import SimulatorAccessor from "./memory";
+
 const Kepler = () => {
-    init().then((instance: InitOutput) => {
-      const countup_simulator = SimulatorFactory.new_countup_simulator(6, 10);
-      const shared_memory = new Float64Array(instance.memory.buffer, countup_simulator.as_mem_ptr(), 60);
-      for(var idx = 0; idx < 60; ++ idx) {
-        console.log(shared_memory[idx]);
-      }
+    init().then(async (instance: InitOutput) => {
+        const simulator = new SimulatorAccessor(
+            SimulatorFactory.new_countup_simulator(10, 100),
+            instance.memory,
+            3,
+            () => { console.log("Now Loading..."); },
+            () => { console.log("Loading Completed!"); }
+        );
+        for(var idx = 0; idx < 6000; ++ idx) {
+            console.log(simulator.getValue());
+            simulator.updateCursor(1);
+        }
     });
 
     return (
