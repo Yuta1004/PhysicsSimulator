dd4c8b20b0e9da3d3678d8f6f737d1ab37e49a2e
NakagamiYuta <guguru0014@gmail.com>
2022-04-26 17:26:14 +0900

    [add](core) デバッグ用にCountUpSimulatorを追加

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -7,6 +7,7 @@ use wasm_bindgen::prelude::*;
 
 use memory::MemManager;
 use simulator::Simulator;
+use simulator::countup::CountUp;
 use simulator::planet::Planet;
 
 #[wasm_bindgen]
@@ -15,6 +16,14 @@ struct SimulatorFactory;
 #[wasm_bindgen]
 #[allow(non_snake_case, dead_code)]
 impl SimulatorFactory {
+    pub fn new_countup_simulator(blocks_num: i32, steps_num: i32, start: i32) -> MemManager {
+        MemManager::new(
+            blocks_num,
+            steps_num,
+            Box::new(Simulator::from(CountUp::new(start)))
+        )
+    }
+
     pub fn new_planet_simulator(blocks_num: i32, steps_num:i32, x: f64, y: f64, v: f64, M: f64, dt: f64) -> MemManager {
         MemManager::new(
             blocks_num,
--- core/src/simulator.rs
+++ core/src/simulator.rs
@@ -1,3 +1,4 @@
+pub mod countup;
 pub mod planet;
 
 use std::collections::HashMap;
