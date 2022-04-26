68badc0974c73aafbe3e49efe9d54637ddece3fa
NakagamiYuta <guguru0014@gmail.com>
2022-04-26 16:11:38 +0900

    [add](core) SimulatorFactoryにPlanetSimulatorを追加

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -8,9 +8,15 @@ extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
 use memory::MemManager;
+use simulator::planet::PlanetSimulator;
 
 #[wasm_bindgen]
 struct SimulatorFactory;
 
 #[wasm_bindgen]
-impl SimulatorFactory { }
+impl SimulatorFactory {
+    pub fn new_planet_simulator(blocks_num: i32, steps_num:i32, x: f64, y: f64, v: f64, M: f64, dt: f64) -> MemManager {
+        let planet = Box::new(PlanetSimulator::new(x, y, v, M, dt));
+        MemManager::new(blocks_num, steps_num, planet)
+    }
+}
