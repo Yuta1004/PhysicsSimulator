cff60c2d0501584cd9724a5fb0f92349cb033615
NakagamiYuta <guguru0014@gmail.com>
2022-04-26 16:47:27 +0900

    [update](core) シミュレータ関連の共通処理部分をSimulator, Calclatorとして独立させた

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,14 +1,13 @@
 pub mod memory;
-pub mod simulator {
-    pub mod planet;
-}
+pub mod simulator;
 
 extern crate wasm_bindgen;
 
 use wasm_bindgen::prelude::*;
 
 use memory::MemManager;
-use simulator::planet::PlanetSimulator;
+use simulator::Simulator;
+use simulator::planet::Planet;
 
 #[wasm_bindgen]
 struct SimulatorFactory;
@@ -16,7 +15,7 @@ struct SimulatorFactory;
 #[wasm_bindgen]
 impl SimulatorFactory {
     pub fn new_planet_simulator(blocks_num: i32, steps_num:i32, x: f64, y: f64, v: f64, M: f64, dt: f64) -> MemManager {
-        let planet = Box::new(PlanetSimulator::new(x, y, v, M, dt));
-        MemManager::new(blocks_num, steps_num, planet)
+        let planet = Simulator::from(Planet::new(x, y, v, M, dt));
+        MemManager::new(blocks_num, steps_num, Box::new(planet))
     }
 }
--- core/src/simulator/planet.rs
+++ core/src/simulator/planet.rs
@@ -1,54 +1,7 @@
-extern crate wasm_bindgen;
-
-use std::collections::HashMap;
-
-use wasm_bindgen::prelude::*;
-
-use crate::memory::ValueGenerator;
-
-#[wasm_bindgen]
-pub struct PlanetSimulator {
-    checkpoints: HashMap<i32, StepData>
-}
-
-impl PlanetSimulator {
-    pub fn new(x: f64, y: f64, v: f64, M: f64, dt: f64) -> PlanetSimulator {
-        let mut checkpoints = HashMap::new();
-        checkpoints.insert(0, StepData::new(x, y, v, M, dt));
-        PlanetSimulator { checkpoints }
-    }
-}
-
-impl ValueGenerator for PlanetSimulator {
-    fn get_step_size(&self) -> i32 {
-        4
-    }
-
-    fn update(&mut self, mem: &mut [f64], base_step: i32, steps: i32) {
-        let mut checkpoint = match self.checkpoints.contains_key(&base_step) {
-            true => self.checkpoints.get(&base_step).unwrap().clone(),
-            false => {
-                let latest_step = *self.checkpoints.keys().filter(|x| x < &&base_step).max().unwrap();
-                let mut checkpoint = self.checkpoints.get(&latest_step).unwrap().clone();
-                for _ in latest_step..base_step+1 {
-                    checkpoint.update();
-                }
-                checkpoint
-            }
-        };
-        for idx in 0..steps {
-            let idx = 4*idx as usize;
-            checkpoint.write(&mut mem[idx..idx+4]);
-            checkpoint.update();
-        }
-        if !self.checkpoints.contains_key(&(base_step+steps-1)) {
-            self.checkpoints.insert(base_step+steps, checkpoint);
-        }
-    }
-}
+use super::Calculator;
 
 #[derive(Clone)]
-struct StepData {
+pub struct Planet {
     x: f64,
     y: f64,
     vx: f64,
@@ -58,11 +11,17 @@ struct StepData {
     dt: f64
 }
 
-impl StepData {
-    fn new(x: f64, y: f64, v: f64, M: f64, dt: f64) -> StepData {
+impl Planet {
+    pub fn new(x: f64, y: f64, v: f64, M: f64, dt: f64) -> Planet {
         let radius = (x.powi(2)+y.powi(2)).sqrt();
         let vy = (1.0/radius).sqrt() * v;
-        StepData { x, y, vx: 0.0, vy, M, dt }
+        Planet { x, y, vx: 0.0, vy, M, dt }
+    }
+}
+
+impl Calculator for Planet {
+    fn get_step_size(&self) -> i32{
+        4
     }
 
     fn update(&mut self) {
