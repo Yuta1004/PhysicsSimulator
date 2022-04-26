d95a2aa0799632f85af7fcdc6e250a0775c2bfd8
NakagamiYuta <guguru0014@gmail.com>
2022-04-27 05:04:58 +0900

    [update](front) Objectsに与えられた更新指示をObjectに通知できるようにした

--- front/src/app/kepler/components/2d/layers/objects.tsx
+++ front/src/app/kepler/components/2d/layers/objects.tsx
@@ -7,49 +7,62 @@ import Object from "./components/object";
 class Objects extends React.Component<any, any> {
     constructor(props: any) {
         super(props);
+        
         this.state = {
             objects: [],
             _dummy: 0
         };
+
+        this.prev = this.prev.bind(this);
+        this.next = this.next.bind(this);
+        this.addSimulator = this.addSimulator.bind(this);
     }
 
     render() {
         return (
             <Layer>
-                { this.state.objects.map((_: SimulatorAccessor, object: Object) => object )}
+                { this.state.objects.map((object: [SimulatorAccessor, JSX.Element ,React.RefObject<Object>]) =>
+                    object[1]
+                )}
             </Layer>
         );
     }
 
     public addSimulator(simulator: SimulatorAccessor, name: string, color: string) {
+        const objects = this.state.objects;
         const value = simulator.getValue();
+        const objectRef = React.createRef<Object>();
         const object = <Object
             name={name}
             color={color}
             x={value[0]}
             y={value[1]}
+            ref={objectRef}
         />;
-        const objects = this.state.objects;
-        objects.push([simulator, object]);
+        objects.push([simulator, object, objectRef]);
         this.setState({ objects: objects });
     }
 
     public prev() {
-        this.state.objects.map((simulator: SimulatorAccessor, object: Object) => {
-            simulator.updateCursor(-1);
-            const value = simulator.getValue();
-            object.update(value[0], value[1]);
+        this.state.objects.map((object: [SimulatorAccessor, JSX.Element, React.RefObject<Object>]) => {
+            if (object[2].current !== null) {
+                object[0].updateCursor(-1);
+                const value = object[0].getValue();
+                object[2].current.update(value[0], value[1]);
+            }
         });
         this.setState({ _dummy: this.state._dummy-1 })
     }
 
     public next() {
-        this.state.objects.map((simulator: SimulatorAccessor, object: Object) => {
-            simulator.updateCursor(1);
-            const value = simulator.getValue();
-            object.update(value[0], value[1]);
+        this.state.objects.map((object: [SimulatorAccessor, JSX.Element, React.RefObject<Object>]) => {
+            if (object[2].current !== null) {
+                object[0].updateCursor(1);
+                const value = object[0].getValue();
+                object[2].current.update(value[0], value[1]);
+            }
         });
-        this.setState({ _dummy: this.state._dummy+1 })
+        this.setState({ _dummy: this.state._dummy-1 })
     }
 }
 
