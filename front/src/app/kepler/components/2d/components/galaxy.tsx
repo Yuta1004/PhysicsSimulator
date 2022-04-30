852f9c56a5f3a3f06efc3c875fd113a1a9dc19d0
NakagamiYuta <guguru0014@gmail.com>
2022-05-01 04:45:17 +0900

    [add](front) ボタンを押すことでGalaxyを表示できるようにした

--- front/src/app/kepler/components/2d/components/galaxy.tsx
+++ front/src/app/kepler/components/2d/components/galaxy.tsx
@@ -12,10 +12,16 @@ export default class Galaxy extends React.Component<any, any> {
                     border: "1px solid black",
                     borderRadius: "30px",
                     width: "100%",
-                    padding: "5px 15px",
+                    padding: "15px",
                     background: "#ccc"
                 }}>
                     <h3>天体画像サイトの表示</h3>
+                    <button
+                        style={{ width: "100%" }}
+                        onClick={this.props.cancelCallback}
+                    >
+                        閉じる
+                    </button>
                 </div>
             </div>
         );
