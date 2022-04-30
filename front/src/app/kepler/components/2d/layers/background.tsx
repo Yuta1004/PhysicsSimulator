c39cf8295b3b81179c5c5bcccd82482ee821bcfc
NakagamiYuta <guguru0014@gmail.com>
2022-04-30 20:03:55 +0900

    [fix](front) 描画範囲外に目盛りが描画されていたのを直した

--- front/src/app/kepler/components/2d/layers/background.tsx
+++ front/src/app/kepler/components/2d/layers/background.tsx
@@ -7,16 +7,17 @@ export default class Background extends React.Component {
     }
 
     render() {
+        const lines = 66;
         return (
             <Layer>
                 <Rect x={-5000} y={-5000} width={10000} height={10000} fill="#f2f2f2"/>
-                {[...Array(100)].map((_, val) => {
-                    if (val !== 50) {
+                {[...Array(lines)].map((_, val) => {
+                    if (val !== lines/2-1) {
                         return <Group key={val}>
-                            <Line points={[(val-50)*150, -5000, (val-50)*150, 5000]} stroke="#c0c0c0" strokeWidth={1}/>
-                            <Line points={[5000, (val-50)*150, -5000, (val-50)*150]} stroke="#c0c0c0" strokeWidth={1}/>
-                            <Text text={(val-50)*5+""} x={(val-50)*150+10} y={-20} fontSize={20}/>
-                            <Text text={(val-50)*5+""} x={10} y={-(val-50)*150+10} fontSize={20}/>
+                            <Line points={[(val-lines/2+1)*150, -5000, (val-lines/2+1)*150, 5000]} stroke="#c0c0c0" strokeWidth={1}/>
+                            <Line points={[5000, (val-lines/2+1)*150, -5000, (val-lines/2+1)*150]} stroke="#c0c0c0" strokeWidth={1}/>
+                            <Text text={(val-lines/2+1)*5+""} x={(val-lines/2+1)*150+10} y={-20} fontSize={20}/>
+                            <Text text={(val-lines/2+1)*5+""} x={10} y={-(val-lines/2+1)*150+10} fontSize={20}/>
                         </Group>;
                     }
                 }
