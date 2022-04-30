b4cee22316d83188b1c3c7deb54f2c8a3914e8fb
NakagamiYuta <guguru0014@gmail.com>
2022-05-01 06:40:36 +0900

    [add](front) ルーティング追加

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
 import Kepler from './app/kepler/kepler';
 import init, { InitOutput } from '@kepler-core/kepler-core';
@@ -7,6 +8,11 @@ init().then((instance: InitOutput) => {
     const container = document.getElementById("root");
     const root = createRoot(container!);
     root.render(
-        <Kepler memory={instance.memory}/>
+        <BrowserRouter>
+            <Routes>
+                <Route path="" element={<Navigate replace to="/kepler"/>}/>
+                <Route path="kepler" element={<Kepler memory={instance.memory}/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 });
